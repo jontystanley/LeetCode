46d1159a54131eb31217392f3eb1aca5e430531a
Jayant Jonathan Stanley <jontystanley21@gmail.com>
2021-08-23 14:42:40 +0530

    Time: 4 ms (77.01%), Space: 10.8 MB (53.48%) - LeetHub

--- orderly-queue/orderly-queue.cpp
+++ orderly-queue/orderly-queue.cpp
@@ -4,11 +4,11 @@ public:
         if(k>1){
             sort(begin(s),end(s));
             return s;
-           }
-        string res=s;
-        for(int i=1;i<s.size();i++){
-            res= min(res,s.substr(i)+s.substr(0,i));
         }
-        return res;
+        string mins=s;
+        for(int i=1;i<s.size();i++){
+            mins=min(mins,s.substr(i)+s.substr(0,i));
+        }
+        return mins;
     }
 };
