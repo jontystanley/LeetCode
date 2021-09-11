38336e3e61acc7eaa5b9db14ff25197dfb759cf4
Jayant Jonathan Stanley <jontystanley21@gmail.com>
2021-09-11 12:30:05 +0530

    Time: 3 ms (63.21%), Space: 7.4 MB (51.75%) - LeetHub

---
+++ arithmetic-slices/arithmetic-slices.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int ans; vector<int>dp(nums.size());
+        for(int i=2;i<nums.size();i++){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                dp[i]=dp[i-1]+1;
+                ans+=dp[i];
+            }
+        }
+        return ans;
+    }
+};
