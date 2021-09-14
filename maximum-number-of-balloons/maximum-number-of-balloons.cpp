e1a09fc08b14601a45eef0e2e5e8348b8f453732
Jayant Jonathan Stanley <jontystanley21@gmail.com>
2021-09-14 09:36:02 +0530

    Time: 4 ms (74.91%), Space: 6.8 MB (6.04%) - LeetHub

--- maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char,int>u;
-        for(char ch:text)
-            if(ch=='b' || ch=='a' || ch=='l' || ch=='o' || ch=='n') u[ch]++;
-        int min1=INT_MAX,min2=INT_MAX;
-        for(char ch:text){
-            if(ch=='b' || ch=='a' || ch=='n')  min1=min(min1,u[ch]);
-            if(ch=='l' || ch=='o')  min2=min(min2,u[ch]/2);
-        }
-        if(min1==INT_MAX || min2==INT_MAX)return 0;
-        else return min(min1,min2);
+        for(char ch:text)u[ch]++;
+        return min({u['b'],u['a'],u['l']/2,u['o']/2,u['n']});
     }
 };
